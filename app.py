bbcad6713fc64ca506c7cf8acb6560e7034cde91
Esmail Essam <esmailessam6@gmail.com>
2025-02-24 15:23:20 +0200

    add his

--- app.py
+++ app.py
@@ -4,30 +4,33 @@ from modules.crew import ai_crew
 # Configure page
 st.set_page_config(page_title="Sales Chatbot", page_icon="🏷")
 
-def main():
-    if 'messages' not in st.session_state:
-        st.session_state.messages = []
-    
-    # Get user input
-    user_input = st.chat_input("What is your question?")
-    
-    if user_input:
-        # Add user message to chat history
-        st.session_state.messages.append({"role": "user", "content": user_input})
-        
-        # Display user message
-        with st.chat_message("user"):
-            st.markdown(user_input)
-        
-        # Get AI response
-        with st.chat_message("assistant"):
-            message_placeholder = st.empty()
-            result = ai_crew.kickoff(inputs={
-              'user_input': user_input
-              })
-            
-            message_placeholder.markdown(result.raw)
+if 'messages' not in st.session_state:
+    st.session_state.messages = []
+
+# Display chat history
+for message in st.session_state.messages:
+    print(message)
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
 
 
-if __name__ == "__main__":
-    main()
+# Get user input
+user_input = st.chat_input("What is your question?")
+
+if user_input:
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": user_input})
+    
+    # Display user message
+    with st.chat_message("user"):
+        st.markdown(user_input)
+    
+    # Get AI response
+    with st.chat_message("assistant"):
+        result = ai_crew.kickoff(inputs={
+          'user_input': user_input
+          })
+        
+        st.markdown(result.raw)
+    # Add assistant response to chat history
+    st.session_state.messages.append({"role": "assistant", "content": result.raw})
