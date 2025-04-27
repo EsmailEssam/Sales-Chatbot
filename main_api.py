2b94bbfda284ebed339c81876f08827588109074
veto2922 <veto2922@gmail.com>
2025-04-27 15:07:49 +0300

    add base_router in routes floder

--- main_api.py
+++ main_api.py
@@ -1,10 +1,12 @@
+from dotenv import load_dotenv
 from fastapi import FastAPI
+
+load_dotenv()
+from routes.base_router import base_router
+
+
 
 app = FastAPI()
 
 
-@app.get('/welcome')
-def welcome():
-    return{
-        "message" : 'Hello Gouds'
-    }
+app.include_router(base_router)
